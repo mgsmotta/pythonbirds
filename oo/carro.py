d8a29034e7472ef617282d75ec98e56a801e0b27
mgsmotta <mgsmotta@gmail.com>
2021-09-29 15:35:04 +0100

    Composição

---
+++ oo/carro.py
@@ -0,0 +1,95 @@
+"""
+Você deve criar uma classe carro que vai possuir dois atributos compostos por outras duas classes:
+1) Motor
+2) Direção
+
+O Motor terá a responsabilidade de controlar a velocidade
+Ele oferece os seguintes atributos:
+a) Atributo de dado velocidade
+b) Método acelerar, que deverá incrementar a velocidade de uma unidade
+c) Método frear, que deverá decrementar a velocidade em duas unidades
+
+A direcão terá a responsabilidade de controlar a direção.
+Ela oferece os seguintes atributos:
+a) Valor de direção com valores possiveis: Norte, Sul, Leste e Oeste
+b) Método girar a Direita => N, L, S, O
+c) Método girar a Esquerda => N, O, S, L
+
+   N
+O     L
+   S
+
+Exemplo:
+
+motor = Motor()
+motor.velocidade
+0
+motor.acelerar()
+motor.velocidade
+1
+motor.acelerar()
+motor.velocidade
+2
+motor.acelerar()
+motor.velocidade
+3
+motor.frear()
+motor.velocidade
+1
+motor.frear()
+motor.velocidade
+0 # nunca menor que 0
+
+# testando direção
+direcao = Direcao()
+direcao.valor
+'Norte'
+direcao.girar_a_direita()
+direcao.valor
+'Leste'
+direcao.girar_a_direita()
+direcao.valor
+'Sul'
+direcao.girar_a_direita()
+direcao.valor
+'Oeste'
+direcao.girar_a_direita()
+direcao.valor
+'Norte'
+direcao.girar_a_esquerda()
+direcao.valor
+'Oeste'
+direcao.girar_a_esquerda()
+direcao.valor
+'Sul'
+direcao.girar_a_esquerda()
+direcao.valor
+'Leste'
+direcao.girar_a_esquerda()
+direcao.valor
+'Norte'
+
+carro = Carro(direcao, motor)
+carro.calcular_velocidade()
+0
+carro.acelerar()
+carro.calcular_velocidade()
+1
+carro.acelerar()
+carro.calcular_velocidade()
+2
+carro.frear()
+carro.calcular_velocidade()
+0
+carro.calcular_direcao()
+'Norte'
+carro.girar_a_direita()
+carro.calcular_direcao()
+'Leste'
+carro.girar_a_esquerda()
+carro.calcular_direcao()
+'Norte'
+carro.girar_a_esquerda()
+carro.calcular_direcao()
+'Oeste'
+"""

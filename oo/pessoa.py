6e5d061c948b6796829c887c7755fb764c26c624
mgsmotta <mgsmotta@gmail.com>
2021-09-29 14:12:43 +0100

    Criado metodo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -11,6 +11,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá! {id(self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
+
 
 if __name__ == '__main__':
     mario = Pessoa(nome='Mario')
@@ -36,3 +44,5 @@ if __name__ == '__main__':
     print(sylvio.olhos)
     print(mario.olhos)
     print(id(Pessoa.olhos), id(sylvio.olhos), id(mario.olhos))
+    print(Pessoa.metodo_estatico(), sylvio.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), sylvio.nome_e_atributos_de_classe())
